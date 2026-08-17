c1c495dd9d5170eb366a3fb85cf2a6e98ed2c129
luanagualiume <luanagualiume07@gmail.com>
2026-08-16 22:42:14 -0300

    Add models e troca da tela inicial

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -7,11 +7,10 @@ namespace MauiAppMinhasCompras
         public App()
         {
             InitializeComponent();
+            //MainPage = new AppShell();
+            MainPage = new NavigationPage(new Views.ListaProduto());
         }
 
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
-        }
+       
     }
 }
